7db78fad2b56354df06b65955c3c11f28cef2a1d
fatimapanhwar <153510549+fatimapanhwar@users.noreply.github.com>
2023-12-30 20:49:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// var num1 = 9;
+// var num2 = 7;
+
+// sum = num1 + num2 ;
+// sub = num1 - num2 ;
+// mul = num1 * num2 ;
+// div = num1 / num2 ;
+  
+
+// var content = `
+
+// <ul>
+//         <li> 🕷️summation of two is : <span> ${ sum} </span> </li>
+//         <li> 👽subtraction of two is : <span> ${ sub}  </span> </li>
+//         <li> 🤢 division of two is :  <span> ${ div} </span> </li>
+//         <li>  👻multiplication of two is :<span> ${ mul} </span> </li>
+//     </ul>
+
+
+// `;
+// document.body.innerHTML = content ;
